2ffdbf3643260b3dd5458ded78e4ba6caeed9427
Out-Set <swk802@gmail.com>
2022-11-29 14:29:22 +0530

    on 29-Nov-2k22

--- App1/src/main/java/com/example/App1/DBUser.java
+++ App1/src/main/java/com/example/App1/DBUser.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class DBUser {
     
-    private List<User> userTable = new ArrayList<User>();
+    private ArrayList<User> userTable = new ArrayList<User>();
 
     public DBUser(){
         userTable.add(new User(1, "Ramu", 30));
@@ -18,9 +18,30 @@ public class DBUser {
 
     public User getAUser(int id){
         for(User user : userTable){
-            if(user.getId() == id)
-                return user;
+            if(user.getId() == (id)) return user;
         }
         return null;
+    }
+
+    public User getAUserByName(String name){
+        for(User user : userTable){
+            if(user.getName().equals(name)) return user;
+        }
+        return null;
+    }
+
+    public boolean addUser(User user){
+        userTable.add(user);
+        return true; 
+    }
+
+    public boolean deleteAUser(int id){
+        for(User user : userTable){
+            if(user.getId() == (id)){
+                userTable.remove(user);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- App1/src/main/java/com/example/App1/SampleController.java
+++ App1/src/main/java/com/example/App1/SampleController.java
@@ -1,13 +1,19 @@
 package com.example.App1;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class SampleController {
     
     DBUser db = new DBUser();
 
+    // GET APIs
     // http://localhost:8080/users   --> Get
     @GetMapping("/users")
     public List<User> getAllUsers() {
@@ -18,5 +24,22 @@ public class SampleController {
     @GetMapping("/users/{id}")
     public User getAUser(@PathVariable int id) {
         return db.getAUser(id);
+    }
+
+    @GetMapping("/user")
+    public User getAUser(@RequestParam String q) {
+        return db.getAUserByName(q);
+    } 
+
+
+    // POST APIs 
+    @PostMapping("/users")
+    public boolean createUser(@RequestBody User user){
+        return db.addUser(user);
+    }
+
+    @DeleteMapping("/users")
+    public boolean deleteAUser(@PathVariable int id){
+        return db.deleteAUser(id);
     }
 }
