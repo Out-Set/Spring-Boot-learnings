9b45104cfab77ae12535442cedbd579d60279e06
swk802@gmail.com <swk802@gmail.com>
2022-11-30 16:55:10 +0530

    on Nov-30-2k22

--- App1/src/main/java/com/example/App1/SampleController.java
+++ App1/src/main/java/com/example/App1/SampleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SampleController {
     
     DBUser db = new DBUser();
